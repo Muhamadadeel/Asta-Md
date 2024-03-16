84ede08195b1fc002a292e43c955e23f30e378fb
Astropeda <astromedia0010@gmail.com>
2024-03-16 06:26:05 +0100

    Asta

--- index.js
+++ index.js
@@ -1,55 +1,38 @@
-const {
-  DisconnectReason,
-  useMultiFileAuthState,
-} = require("@whiskeysockets/baileys");
-const useMongoDBAuthState = require("./mongoAuthState");
+const { DisconnectReason, useMultiFileAuthState } = require("@whiskeysockets/baileys");
 const makeWASocket = require("@whiskeysockets/baileys").default;
-const mongoURL = "mongodb+srv://astromedia0010:beka10beka10@cluster0.pxc8ulo.mongodb.net/";
-const { MongoClient } = require("mongodb");
 
 async function connectionLogic() {
-  const mongoClient = new MongoClient(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  await mongoClient.connect();
-  // const { state, saveCreds } = await useMultiFileAuthState("auth_info_baileys");
-  const collection = mongoClient
-    .db("whatsapp_api")
-    .collection("auth_info_baileys");
-  const { state, saveCreds } = await useMongoDBAuthState(collection);
+  const { state, saveCreds } = await useMultiFileAuthState("auth_info_baileys");
   const sock = makeWASocket({
-    // can provide additional config here
     printQRInTerminal: true,
     auth: state,
   });
 
   sock.ev.on("connection.update", async (update) => {
     const { connection, lastDisconnect, qr } = update || {};
-
     if (qr) {
       console.log(qr);
-      // write custom logic over here
     }
-
     if (connection === "close") {
       const shouldReconnect =
-        lastDisconnect?.error?.output?.statusCode !==
-        DisconnectReason.loggedOut;
-
+        lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut;
       if (shouldReconnect) {
         connectionLogic();
       }
     }
   });
 
-  sock.ev.on("messages.update", (messageInfo) => {
-    console.log(messageInfo);
+  sock.ev.on("messages.upsert", async (messageInfoUpsert) => {
+    const messageInfo = messageInfoUpsert.messages[0];
+    if (messageInfo.key.remoteJid === "status@broadcast") return;
+
+    // Check if the message is a text message and contains "hello"
+    if (messageInfo.message?.conversation?.toLowerCase().includes("hello")) {
+      const chatId = messageInfo.key.remoteJid;
+      await sock.sendMessage(chatId, { text: "Hello, there!" });
+    }
   });
 
-  sock.ev.on("messages.upsert", (messageInfoUpsert) => {
-    console.log(messageInfoUpsert);
-  });
   sock.ev.on("creds.update", saveCreds);
 }
 
