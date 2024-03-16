a61f6b316271cf49cb822d4b8a1a62280263c380
Astropeda <astromedia0010@gmail.com>
2024-03-16 06:51:58 +0100

    Adjusted

--- index.js
+++ index.js
@@ -22,16 +22,60 @@ async function connectionLogic() {
     }
   });
 
-  sock.ev.on("messages.upsert", async (messageInfoUpsert) => {
-    const messageInfo = messageInfoUpsert.messages[0];
-    if (messageInfo.key.remoteJid === "status@broadcast") return;
+  sock.ev.on('messages.upsert', async (messageUpdate) => {
+    const messages = messageUpdate.messages;
+for (const message of messages) {
+  if (!message.message || message.key.fromMe) continue;
+  const text = message.message.conversation.toLowerCase(); // Convert the text to lowercase
+  const prefix = /^[.!#/$,]/i; // Regular expression to match any of the prefixes, case-insensitive
 
-    // Check if the message is a text message and contains "hello"
-    if (messageInfo.message?.conversation?.toLowerCase().includes("hello")) {
-      const chatId = messageInfo.key.remoteJid;
-      await sock.sendMessage(chatId, { text: "Hello, there!" });
+  if (prefix.test(text)) {
+    const command = text.split(' ')[0].slice(1); // No need to convert to lowercase again
+    const args = text.slice(command.length + 2).trim();
+            switch (command) {
+                //menu
+                case 'menu':
+  const menuText = `ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴍᴇɴᴜ
+
+  1. Ping
+  2. Echo
+  3. Option 3
+
+  *2024 ᴀꜱᴛʀᴏᴘᴇᴅᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ*.`;
+
+  sock.sendMessage(message.key.remoteJid, { text: menuText });
+  break;
+
+  case 'ping':
+                    const pingStart = Date.now()
+                    await sock.sendMessage(message.key.remoteJid, {
+                        text: 'Pong!'
+                    })
+                    const pingEnd = Date.now()
+                    const pingTime = pingEnd - pingStart
+                    await sock.sendMessage(message.key.remoteJid, {
+                        text: `Ping time: ${pingTime}ms`
+                    })
+                    break
+
+                    case 'echo':
+                      sock.sendMessage(message.key.remoteJid, {
+                          text: args
+                      })
+                      break
+
+case '3':
+  sock.sendMessage(message.key.remoteJid, { text: 'You selected Option 3' });
+  break;
+
+default:
+  sock.sendMessage(message.key.remoteJid, { text: 'Invalid option. Please try again.' });
+  break
+                
+            }
+        }
     }
-  });
+})
 
   sock.ev.on("creds.update", saveCreds);
 }
