db7d4b40203db17843ba702102a1968104970360
Astropeda <astromedia0010@gmail.com>
2024-03-16 09:47:55 +0100

    Asta

---
+++ config.js
@@ -0,0 +1,69 @@
+const fs = require('fs-extra')
+const randomPort = Math.floor(Math.random() * (65535 - 1024) + 1024);
+if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/config.env' })
+
+
+//═══════[Required Variables]════════\\
+global.owner = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, ''): '2348039607375';
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://Suhail_Md:Suhail_Md@cluster1.0tlyzhm.mongodb.net/?retryWrites=true&w=majority" ;
+global.port= randomPort  ;
+global.audio = '' ; 
+global.video = '' ;
+global.blockJids = process.env.BLOCK_JID ||'120363023983262391@g.us' ;
+global.allowJids = process.env.ALLOW_JID ||'120363022922797633@g.us' ;
+global.email = 'samsamsun789@gmail.com' ;
+global.github = 'https://github.com/Astropeda/whatsapp-bot' ;
+global.location = 'Lahore Pakistan' ;
+global.timezone  = process.env.TIME_ZONE || 'Asia/Karachi'
+global.ownernames = 'Astropeda'
+global.packname = '© Asta'
+global.creator = 'Astro'
+global.waitTime = null;
+global.gurl = 'https://youtube.com/c/SuhailTechInfo' ; // add your username
+global.sudo =  process.env.SUDO || "923184474176" ;
+global.devs = "923184474176"; //Dont change it From here
+global.website = 'https://github.com/Astropeda/whatsapp-bot' ; //wa.me/+91000000000000
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/NpA3ZsJ.jpeg' ;
+module.exports = {
+  sessionName: process.env.SESSION_ID || '',      //Put Your Session Id Here
+  botname: process.env.BOT_NAME || 'ASTA',
+  botbgm: process.env.BOT_BGM || 'false',
+  ownername:  process.env.OWNER_NAME || `Astro`,
+  author:  process.env.PACK_AUTHER || '', 
+  read_status : process.env.AUTO_READ_STATUS || 'true',
+  save_status : process.env.AUTO_SAVE_STATUS || 'true',
+  packname:  process.env.PACK_NAME || "Asta" ,
+  autoreaction: process.env.AUTO_REACTION || 'false',
+  antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokeyuntillYouPutAnWordHere',
+  alwaysonline: process.env.ALWAYS_ONLINE || 'false',
+  antifake :   process.env.FAKE_COUNTRY_CODE ||'212',
+  readmessage: process.env.READ_MESSAGE || 'false',
+  readcmds : process.env.READ_COMMANDS || 'false',
+  HANDLERS: process.env.PREFIX || ',',
+  warncount : process.env.WARN_COUNT || 3,
+  disablepm: process.env.DISABLE_PM || "true",
+  MsgsInLog:process.env.MSGS_IN_LOG ||'false',
+  pmMsgsInLog:process.env.PM_MSGS_IN_LOGS ||'false',
+  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'false',
+  antilink_values: process.env.ANTILINK_VALUES || 'https://,chat.whatsapp.com',
+  antilinkaction: process.env.BRANCH || 'remove',
+  BRANCH: process.env.BRANCH || 'main',
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY,
+  REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "Nh4iTxEH5k3vmHugFqoYktGM",
+  caption :process.env.CAPTION || "*Astropeda Team*",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY ||'' ,
+  VERSION: process.env.VERSION || 'v.0.0.3',
+  LANG: process.env.THEME || 'SUHAIL',
+  menu : process.env.MENU || '', /**  Available @MENU @Schemes 1: Aztec_Md, 2: A17_Md, 3: Suhail-Md Default ---------- If Not Choose then it Randomely Pic One Of Them Each time **/
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || 'public'
+};
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+    delete require.cache[file]
+	require(file)
+})
