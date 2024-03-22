89a3bb2ed1ed7e2411eeac6bf4f6ccbfe16b2b26
Astropeda <astromedia0010@gmail.com>
2024-03-22 19:40:00 +0100

    ꜰɪɴᴀʟ ᴘᴀᴛᴄʜ

--- plugins/antivv.js
+++ plugins/antivv.js
@@ -1,84 +1,61 @@
-const {
-    smd,
-    tlang,
-    botpic,
-    prefix,
-    Config,
-    bot_
-  } = require("../lib");
-  let bgmm = false;
-  smd({
-    cmdname: "antiviewonce",
-    alias: ["antivv"],
-    desc: "turn On/Off auto viewOnce Downloder",
-    fromMe: true,
-    type: "general",
-    use: "<on/off>",
-    filename: __filename
-  }, async (_0x5c3dd1, _0x543e4e) => {
-    try {
-      bgmm = (await bot_.findOne({
-        id: "bot_" + _0x5c3dd1.user
-      })) || (await bot_.new({
-        id: "bot_" + _0x5c3dd1.user
-      }));
-      let _0x446f76 = _0x543e4e.toLowerCase().split(" ")[0].trim();
-      if (_0x446f76 === "on" || _0x446f76 === "enable" || _0x446f76 === "act") {
-        if (bgmm.antiviewonce === "true") {
-          return await _0x5c3dd1.reply("`AntiViewOnce already enabled!`");
-        }
-        await bot_.updateOne({
-          id: "bot_" + _0x5c3dd1.user
-        }, {
-          antiviewonce: "true"
-        });
-        return await _0x5c3dd1.reply("`AntiViewOnce Succesfully enabled`");
-      } else if (_0x446f76 === "off" || _0x446f76 === "disable" || _0x446f76 === "deact") {
-        if (bgmm.antiviewonce === "false") {
-          return await _0x5c3dd1.reply("`AntiViewOnce already disabled`");
-        }
-        await bot_.updateOne({
-          id: "bot_" + _0x5c3dd1.user
-        }, {
-          antiviewonce: "false"
-        });
-        return await _0x5c3dd1.reply("`AntiViewOnce Succesfully deactivated`");
-      } else {
-        return await _0x5c3dd1.send("`_Use on/off to enable/disable antiViewOnce!_`");
-      }
-    } catch (_0x4bb48d) {
-      await _0x5c3dd1.error(_0x4bb48d + "\n\nCommand: AntiViewOnce ", _0x4bb48d);
-    }
-  });
-  smd({
-    on: "viewonce"
-  }, async (_0x4a4a25, _0x1400fa) => {
-    try {
-      if (!bgmm) {
-        bgmm = await bot_.findOne({
-          id: "bot_" + _0x4a4a25.user
-        });
-      }
-      if (bgmm && bgmm.antiviewonce && bgmm.antiviewonce === "true") {
-        let _0x52bb9a = {
-          key: {
-            ..._0x4a4a25.key
-          },
-          message: {
-            conversation: "`[VIEWONCE DETECTED] downloading!`"
-          }
-        };
-        let _0x58b72c = await _0x4a4a25.bot.downloadAndSaveMediaMessage(_0x4a4a25.msg);
-        await _0x4a4a25.bot.sendMessage(_0x4a4a25.from, {
-          [_0x4a4a25.mtype2.split("Message")[0]]: {
-            url: _0x58b72c
-          },
-          caption: _0x4a4a25.body
-        }, {
-          quoted: _0x52bb9a
-        });
-      }
-    } catch (_0x6010c1) {
-      console.log("error while getting antiviewOnce media\n, ", _0x6010c1);
-    }
-  });
+const { smd, tlang, botpic, prefix, Config, bot_ } = require("../lib");
+
+let antiViewOnceEnabled = false;
+
+smd({
+ cmdname: "antiviewonce",
+ alias: ["antivv"],
+ desc: "turn On/Off auto viewOnce Downloader",
+ fromMe: true,
+ type: "general",
+ use: "<on/off>",
+ filename: __filename
+}, async (context, query) => {
+ try {
+   let botData = (await bot_.findOne({ id: "bot_" + context.user })) || (await bot_.new({ id: "bot_" + context.user }));
+   let action = query.toLowerCase().split(" ")[0].trim();
+
+   if (action === "on" || action === "enable" || action === "act") {
+     if (botData.antiviewonce === "true") {
+       return await context.reply("`AntiViewOnce already enabled!`");
+     }
+     await bot_.updateOne({ id: "bot_" + context.user }, { antiviewonce: "true" });
+     return await context.reply("`AntiViewOnce Successfully enabled`");
+   } else if (action === "off" || action === "disable" || action === "deact") {
+     if (botData.antiviewonce === "false") {
+       return await context.reply("`AntiViewOnce already disabled`");
+     }
+     await bot_.updateOne({ id: "bot_" + context.user }, { antiviewonce: "false" });
+     return await context.reply("`AntiViewOnce Successfully deactivated`");
+   } else {
+     return await context.send("`_Use on/off to enable/disable antiViewOnce!_`");
+   }
+ } catch (error) {
+   await context.error(error + "\n\nCommand: AntiViewOnce ", error);
+ }
+});
+
+smd({
+ on: "viewonce"
+}, async (context, messageData) => {
+ try {
+   if (!antiViewOnceEnabled) {
+     antiViewOnceEnabled = await bot_.findOne({ id: "bot_" + context.user });
+   }
+
+   if (antiViewOnceEnabled && antiViewOnceEnabled.antiviewonce && antiViewOnceEnabled.antiviewonce === "true") {
+     let quotedMessage = {
+       key: { ...context.key },
+       message: { conversation: "`[VIEWONCE DETECTED] downloading!`" }
+     };
+
+     let downloadedMedia = await context.bot.downloadAndSaveMediaMessage(context.msg);
+     await context.bot.sendMessage(context.from, {
+       [context.mtype2.split("Message")[0]]: { url: downloadedMedia },
+       caption: context.body
+     }, { quoted: quotedMessage });
+   }
+ } catch (error) {
+   console.log("error while getting antiviewOnce media\n, ", error);
+ }
+});
