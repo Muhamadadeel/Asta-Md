d58e22fb6344feef1267f6246a6c56a2da971b8e
Astropeda <astromedia0010@gmail.com>
2024-03-22 19:11:17 +0100

    ꜰɪɴᴀʟ ᴘᴀᴛᴄʜ

--- plugins/_plugins.js
+++ plugins/_plugins.js
@@ -14,78 +14,79 @@ const {
   smdBuffer
 } = require("../lib");
 let s_ser = true;
+
 smd({
-  cmdname: "restart",
-  info: "To restart bot",
-  type: "tools",
-  fromMe: s_ser,
-  filename: __filename
-}, async _0x514d3c => {
-  const {
-    exec: _0x1912df
-  } = require("child_process");
-  _0x514d3c.reply("Restarting");
-  _0x1912df("pm2 restart all");
+ cmdname: "restart",
+ info: "To restart bot",
+ type: "tools",
+ fromMe: s_ser,
+ filename: __filename
+}, async (context) => {
+ const { exec } = require("child_process");
+ context.reply("Restarting");
+ exec("pm2 restart all");
 });
+
 smd({
-  cmdname: "shutdown",
-  info: "To shutdown bot",
-  type: "tools",
-  fromMe: s_ser,
-  filename: __filename
-}, async _0x514d3c => {
-  const {
-    exec: _0x1912df
-  } = require("child_process");
-  _0x514d3c.reply("shuttingdown");
-  _0x1912df("pm2 stop all");
+ cmdname: "shutdown",
+ info: "To shutdown bot",
+ type: "tools",
+ fromMe: s_ser,
+ filename: __filename
+}, async (context) => {
+ const { exec } = require("child_process");
+ context.reply("shutting down");
+ exec("pm2 stop all");
 });
+
 smd({
-  cmdname: "plugins",
-  alias: ["plugin"],
-  type: "owner",
-  info: "Shows list of all externally installed modules",
-  fromMe: s_ser,
-  filename: __filename,
-  use: "<name>"
-}, async (_0x2a10d6, _0x2420b0) => {
-  try {
-    let _0x4e5e2e = await plugins(_0x2a10d6, "plugins", _0x2420b0);
-    return await _0x2a10d6.send(!_0x4e5e2e ? "*_There's no plugin install in " + Config.botname + "_*" : !_0x2420b0 ? "*All Installed Modules are:-*\n\n" + _0x4e5e2e : _0x4e5e2e);
-  } catch (_0x21e335) {
-    _0x2a10d6.error(_0x21e335 + " \n\ncmdName plugins\n");
-  }
+ cmdname: "plugins",
+ alias: ["plugin"],
+ type: "owner",
+ info: "Shows list of all externally installed modules",
+ fromMe: s_ser,
+ filename: __filename,
+ use: "<name>"
+}, async (context, query) => {
+ try {
+   let pluginsList = await plugins(context, "plugins", query);
+   return await context.send(!pluginsList ? `*_There's no plugin install in ${Config.botname}_*` : !query ? `*All Installed Modules are:-*\n\n${pluginsList}` : pluginsList);
+ } catch (error) {
+   context.error(error + " \n\ncmdName plugins\n");
+ }
 });
+
 smd({
-  pattern: "remove",
-  alias: ["uninstall"],
-  type: "owner",
-  info: "removes external modules.",
-  fromMe: s_ser,
-  filename: __filename,
-  use: "<plugin name>"
-}, async (_0x1510c9, _0x40e763) => {
-  if (!_0x40e763) {
-    return await _0x1510c9.reply("*_Uhh Please, Provide Me Plugin Name_*");
-  }
-  if (_0x40e763 === "alls") {
-    return await _0x1510c9.reply(await plugins("remove", "all", __dirname));
-  }
-  try {
-    await _0x1510c9.send(await plugins(_0x1510c9, "remove", _0x40e763, __dirname), {}, "", _0x1510c9);
-  } catch {}
+ pattern: "remove",
+ alias: ["uninstall"],
+ type: "owner",
+ info: "removes external modules.",
+ fromMe: s_ser,
+ filename: __filename,
+ use: "<plugin name>"
+}, async (context, query) => {
+ if (!query) {
+   return await context.reply("*_Uhh Please, Provide Me Plugin Name_*");
+ }
+ if (query === "alls") {
+   return await context.reply(await plugins("remove", "all", __dirname));
+ }
+ try {
+   await context.send(await plugins(context, "remove", query, __dirname), {}, "", context);
+ } catch {}
 });
+
 smd({
-  cmdname: "install",
-  type: "owner",
-  info: "Installs external modules..",
-  fromMe: s_ser,
-  filename: __filename,
-  use: "<gist url>"
-}, async (_0xf71b5c, _0x2bdd09) => {
-  let _0x2b0828 = _0x2bdd09 ? _0x2bdd09 : _0xf71b5c.quoted ? _0xf71b5c.quoted.text : "";
-  if (!_0x2b0828.toLowerCase().includes("https")) {
-    return await _0xf71b5c.send("*_Uhh Please, Provide Me Plugin Url_*");
-  }
-  await _0xf71b5c.reply(await plugins(_0xf71b5c, "install", _0x2b0828, __dirname));
+ cmdname: "install",
+ type: "owner",
+ info: "Installs external modules..",
+ fromMe: s_ser,
+ filename: __filename,
+ use: "<gist url>"
+}, async (context, query) => {
+ let url = query ? query : context.quoted ? context.quoted.text : "";
+ if (!url.toLowerCase().includes("https")) {
+   return await context.send("*_Uhh Please, Provide Me Plugin Url_*");
+ }
+ await context.reply(await plugins(context, "install", url, __dirname));
 });
