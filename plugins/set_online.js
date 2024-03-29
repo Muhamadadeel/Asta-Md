e2b8f9f08d49fb1b6bd054a9d297d0d04220ebd1
Astropeda <astromedia0010@gmail.com>
2024-03-29 19:17:08 +0100

    Create set_online.js

---
+++ plugins/set_online.js
@@ -0,0 +1,28 @@
+let { smd, prefix, Config } = require("../lib");
+
+smd(
+  {
+    pattern: "setonline",
+    fromMe: true,
+    alias: "online",
+    desc: "to change online privacy",
+    type: "whatsapp settings",
+  },
+  async (message, match) => {
+    try {
+      if (!match)
+        return await message.send(
+          `_*Example:-* ${Config.HANDLERS}online all`,
+        );
+      const available_privacy = ["all", "match_last_seen"];
+      if (!available_privacy.includes(match))
+        return await message.send(
+          `_action must be *${available_privacy.join("/")}* values_`,
+        );
+      await message.bot.updateOnlinePrivacy(match);
+      await message.send(`_Privacy Updated to *${match}*_`);
+    } catch (e) {
+      message.error(`${e}\n\nCommand : online`, e, false);
+    }
+  },
+);
