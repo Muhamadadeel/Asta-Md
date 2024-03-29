b99535445461f69277546473f96e0dbb5ef19cd1
Astropeda <astromedia0010@gmail.com>
2024-03-29 17:29:59 +0100

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -1,21 +1,16 @@
 const { exec } = require("child_process");
 const { smd } = require("../lib");
-
-// Set the s_ser variable to true (assuming it's a boolean flag)
-let s_ser = true;
-
-// Restart command
 smd(
   {
     cmdname: "restart",
     info: "To restart bot",
     type: "tools",
     fromMe: s_ser,
-    filename: __filename
+    filename: __filename,
   },
   async (message) => {
     const { exec: childExec } = require("child_process");
     message.reply("Restarting");
     childExec("pm2 restart all");
-  }
+  },
 );
