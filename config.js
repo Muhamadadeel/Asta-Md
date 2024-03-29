37d4ad9ca1c8d115e834f60dff7a47cd1b5ea089
Astropeda <astromedia0010@gmail.com>
2024-03-29 18:15:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,47 +1,31 @@
 const fs = require('fs-extra')
 if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
+//=========[IMPORTANT VARS]=========================\\
+global.SESSION_ID = process.env.SESSION_ID ||  "" ;
+global.DATABASE_URL = process.env.DATABASE_URL || "";
+global.mongodb= process.env.MONGODB_URI || "";
+global.appUrl=process.env.APP_URL || "";
+global.timezone= process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
+global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
+global.menuImg= process.env.USER_IMAGES || "https://i.imgur.com/lIo3cM2.jpeg,https://i.imgur.com/OQOH4Gn.jpeg"
+global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "2348039607375";
+global.reset = process.env.RESET   || "false"; 
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.imgur.com/dMwGOUP.jpeg" ;
+//=========[ASTA MD WHATSAPP BOT]====================\\
 
 
-//═══════[Required Variables]════════\\
-global.audio= "" ;  
-global.video= "" ;
-global.port =process.env.PORT
-global.appUrl=process.env.APP_URL || ""                       // put your app url here,
-global.email ="astromedia0010@outlook.com"
-global.location="undefined."
-
-
-global.mongodb= process.env.MONGODB_URI || ""
+//=========[OPTIONAL VARS]==============================\\
 global.allowJids= process.env.ALLOW_JID || "null" 
 global.blockJids= process.env.BLOCK_JID || "null"
-global.DATABASE_URL = process.env.DATABASE_URL || ""
-
-global.timezone= process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
 global.github=process.env.GITHUB|| "https://github.com/Astropeda/Asta-Md";
 global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029VaPGt3QEwEjpBXT4Rv0z/209";
 global.website=process.env.GURL || "https://whatsapp.com/channel/0029VaPGt3QEwEjpBXT4Rv0z/209" ; 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.imgur.com/dMwGOUP.jpeg" ; // SET LOGO FOR IMAGE 
+//=========[ASTA MD WHATSAPP BOT]===============================\\
 
 
-
-global.devs = "2348039607375" // Developer Contact
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "2348039607375";
-
-
-
-
-//========================= [ BOT SETTINGS ] =========================\\
-global.style = process.env.STYLE   || '4'
-global.flush = process.env.FLUSH   || "false";
-global.gdbye = process.env.GOODBYE || "false"; 
-global.wlcm  = process.env.WELCOME || "false";  
-global.warncount = process.env.WARN_COUNT || 3
-global.disablepm = process.env.DISABLE_PM || "false"
-global.disablegroup = process.env.DISABLE_GROUPS || "false", 
-global.MsgsInLog = process.env.MSGS_IN_LOG|| "true"
-global.userImages= process.env.USER_IMAGES || "https://i.imgur.com/lIo3cM2.jpeg,https://i.imgur.com/OQOH4Gn.jpeg"
-global.waPresence= process.env.WAPRESENCE ||  "online" ; 
+//=============[MESSAGE VARS]===================\\
+global.goodbye = process.env.GOODBYE || "false"; 
+global.welcome  = process.env.WELCOME || "false"; 
 global.readcmds = process.env.READ_COMMAND || "false"
 global.readmessage = process.env.READ_MESSAGE || "false"
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "2348039607375,923xxxxxxxx";
@@ -49,18 +33,23 @@ global.read_status = process.env.AUTO_READ_STATUS || "false"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
 global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,923xxxxxxxx";
 global.read_status_from =  process.env.READ_STATUS_FROM  ||  "2348039607375,923xxxxxxxx";
+global.messagetype = process.env.STYLE   || '4'
+global.warncount = process.env.WARN_COUNT || 3
+//===========[ASTA MD WHATSAPP BOT]=====================\\
 
+//===========[OPTIONAL VARS]============================\\
+global.disablegroup = process.env.DISABLE_GROUPS || "false", 
+global.disablepm = process.env.DISABLE_PM || "false"
+global.MsgsInLog = process.env.MSGS_IN_LOG|| "true"
+global.waPresence= process.env.WAPRESENCE ||  "online" ; 
+//=========[ASTA MD WHATSAPP BOT]================\\
+
+//===========[API STUFF DON'T TOUCH ANYWAYS]=============\\
 global.api_smd = "https://api-smd-1.vercel.app"
-global.scan = "https://suhail-md-vtsf.onrender.com/";
-
-
-global.SESSION_ID = process.env.SESSION_ID ||  "" ;
-
-
+global.scan = "https://gnime-qr.onrender.com";
+//===========[ASTA MD WHATSAPP BOT]====================\
 module.exports = {
-
   menu: process.env.MENU || "", // menu1, menu2 ONLY or 1 or 2
-
   HANDLERS: process.env.PREFIX  || ".",
   BRANCH  : process.env.BRANCH  || "main",
   VERSION : process.env.VERSION || "1.3.1",
@@ -69,65 +58,20 @@ module.exports = {
   packname: process.env.PACK_NAME || "♥️",
   botname : process.env.BOT_NAME  || "𝘼𝙎𝙏𝘼 𝙈𝘿",
   ownername:process.env.OWNER_NAME|| "It'x Astropeda",
-
-
   errorChat : process.env.ERROR_CHAT || "",
   KOYEB_API : process.env.KOYEB_API  || "false",
-
   REMOVE_BG_KEY : process.env.REMOVE_BG_KEY  || "",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   HEROKU_APP_NAME:process.env.HEROKU_APP_NAME|| "",
   antilink_values:process.env.ANTILINK_VALUES|| "all",
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
-
   aitts_Voice_Id : process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY  || "",
   WORKTYPE: process.env.WORKTYPE || process.env.MODE|| "private",
   LANG: ( process.env.THEME ||  "Asta"  ).toUpperCase(),
-
-
-
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 global.rank = "updated"
 global.isMongodb = false; 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => { fs.unwatchFile(file);console.log(`Update'${__filename}'`);delete require.cache[file];	require(file); })
- 
-
-// ========================= [ Disables in V.1.2.8 ] ===============================\\  
-  //style : process.env.STYLE || "2",  // put '1' & "2" here to check bot styles
-  //readmessage:process.env.READ_MESSAGE|| "false",
-  //warncount: process.env.WARN_COUNT || 3,
-  //userImages:process.env.USER_IMAGES|| "text",  // SET IMAGE AND VIDEO URL FOR BOT MENUS 
-  //disablepm: process.env.DISABLE_PM || "false",
-  //MsgsInLog: process.env.MSGS_IN_LOG|| "false", // "true"  to see messages , "log" to open logs , "false" to hide logs messages
-  //readcmds:process.env.READ_COMMANDS|| "false", 
-  //alwaysonline:process.env.WAPRESENCE|| "unavailable", // 'unavailable' | 'online' | 'composing' | 'recording' | 'paused'
-  //read_status: process.env.AUTO_READ_STATUS || "false",
-  //save_status: process.env.AUTO_SAVE_STATUS || "false",
