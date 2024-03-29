cad35f8f7f74b28bebd9c2cbdc0de233fab14f99
Astropeda <astromedia0010@gmail.com>
2024-03-29 17:45:24 +0100

    Create uninstall.js

---
+++ plugins/uninstall.js
@@ -0,0 +1,39 @@
+const axios = require("axios");
+const fs = require("fs-extra");
+const { exec } = require("child_process");
+const {
+  plugins,
+  isUrl,
+  smd,
+  tlang,
+  Config,
+  smdJson,
+  smdBuffer,
+} = require("../lib");
+smd(
+  {
+    pattern: "uninstall",
+    alias: ["remove"],
+    type: "owner",
+    info: "removes external modules.",
+    fromMe: s_ser,
+    filename: __filename,
+    use: "<plugin name>",
+  },
+  async (message, pluginName) => {
+    if (!pluginName) {
+      return await message.reply("`Provide Me Plugin Name!`");
+    }
+    if (pluginName === "alls") {
+      return await message.reply(await plugins("remove", "all", __dirname));
+    }
+    try {
+      await message.send(
+        await plugins(message, "remove", pluginName, __dirname),
+        {},
+        "",
+        message,
+      );
+    } catch {}
+  },
+);
