45102cc7f0e371c63af312be5d967d3d36612730
Astropeda <astromedia0010@gmail.com>
2024-03-29 19:16:56 +0100

    Update archive_chat.js

--- plugins/archive_chat.js
+++ plugins/archive_chat.js
@@ -1,20 +1,28 @@
-let {smd , prefix,Config} = require("../lib")
-smd({
-	pattern: 'archive',
-	fromMe: true,
-	desc: 'archive whatsapp chat',
-	type: 'whatsapp'
-}, async (message, match) => {
-    try{
-	const lstMsg = {
-		message: message.message,
-		key: message.key,
-		messageTimestamp: message.messageTimestamp
-	};
-	await message.bot.chatModify({
-		archive: true,
-		lastMessages: [lstMsg]
-	}, message.jid);
-	await message.send('_Archived_')
-}catch(e){ message.error(`${e}\n\nCommand : archive` , e, false) }
-})
+let { smd, prefix, Config } = require("../lib");
+smd(
+  {
+    pattern: "archive",
+    fromMe: true,
+    desc: "archive whatsapp chat",
+    type: "whatsapp",
+  },
+  async (message, match) => {
+    try {
+      const lstMsg = {
+        message: message.message,
+        key: message.key,
+        messageTimestamp: message.messageTimestamp,
+      };
+      await message.bot.chatModify(
+        {
+          archive: true,
+          lastMessages: [lstMsg],
+        },
+        message.jid,
+      );
+      await message.send("_Archived_");
+    } catch (e) {
+      message.error(`${e}\n\nCommand : archive`, e, false);
+    }
+  },
+);
