eb9823e4581b1fd9ac7a68454ce075b0f4244431
Astropeda <astromedia0010@gmail.com>
2024-03-30 17:07:10 +0100

    ᴀsᴛᴀ 𝟷.𝟶.𝟷

--- plugins/wa_link_tag.js
+++ plugins/wa_link_tag.js
@@ -1,17 +1,26 @@
-const {
-    smd
-  } = require("../lib");
-  const cmd = smd;
-  cmd({
+const { smd } = require("../lib");
+const cmd = smd;
+cmd(
+  {
     pattern: "walink",
     desc: "Makes wa me of quoted or mentioned user.",
     category: "user",
-    filename: __filename
-  }, async _0x3186cd => {
+    filename: __filename,
+  },
+  async (_0x3186cd) => {
     try {
-      let _0x3c71d6 = _0x3186cd.reply_message ? _0x3186cd.reply_message.sender : _0x3186cd.mentionedJid[0] ? _0x3186cd.mentionedJid[0] : false;
-      await _0x3186cd.reply(!_0x3c71d6 ? "*Reply or tag the user*" : "https://wa.me/" + _0x3c71d6.split("@")[0]);
+      let _0x3c71d6 = _0x3186cd.reply_message
+        ? _0x3186cd.reply_message.sender
+        : _0x3186cd.mentionedJid[0]
+        ? _0x3186cd.mentionedJid[0]
+        : false;
+      await _0x3186cd.reply(
+        !_0x3c71d6
+          ? "*Reply or tag the user*"
+          : "https://wa.me/" + _0x3c71d6.split("@")[0]
+      );
     } catch (_0x100353) {
       await _0x3186cd.error(_0x100353 + "\n\ncommand : wa", _0x100353, false);
     }
-  });
+  }
+);
