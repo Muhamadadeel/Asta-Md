4bce00b33838fbf9ab364976c82b41cceea23ab4
Astropeda <astromedia0010@gmail.com>
2024-03-30 17:07:01 +0100

    ᴀsᴛᴀ 𝟷.𝟶.𝟷

--- plugins/wa_present.js
+++ plugins/wa_present.js
@@ -1,31 +1,41 @@
-const { 
-    smd,
-    } = require('../lib')
- 
- global.waPresence = process.env.WAPRESENCE && process.env.WAPRESENCE === "online" ? "available" : process.env.WAPRESENCE  ||  "" ;
- global.readmessage = process.env.READ_MESSAGE || global.readmessage || "false";
- global.readmessagefrom = process.env.READ_MESSAGE_FROM || global.readmessagefrom || "false";
- global.readcmds = process.env.READ_COMMAND || global.readcmds || "true"
- 
- let status = false,times = 0;
- smd(
-    { on: "main" },
-    async(message,text,{icmd}) => {
-       try{
-          if(message.status) return
-          if(`${global.readmessagefrom}`.includes(message.senderNum) || ["yes","true","ok","sure"].includes(global.readmessage) || (icmd && ["yes","true","ok","sure"].includes(global.readcmds)) ) message.bot.readMessages([message.key]) 
-       }catch(e){console.log(e)}
- })
- 
- 
- 
- smd(
-    { on: "text" },
-    async(message,text,{icmd}) => {
-       try{
-          if(['unavailable' , 'available' ,'composing','recording','paused'].includes(waPresence)) message.bot.sendPresenceUpdate(waPresence, message.from) 
-          if(message.isAstro && !message.fromMe && !message.text.startsWith("$")  ) message.react("👑")
-       }catch(e){console.log(e)}
- })
- 
- 
+const { smd } = require("../lib");
+
+global.waPresence =
+  process.env.WAPRESENCE && process.env.WAPRESENCE === "online"
+    ? "available"
+    : process.env.WAPRESENCE || "";
+global.readmessage = process.env.READ_MESSAGE || global.readmessage || "false";
+global.readmessagefrom =
+  process.env.READ_MESSAGE_FROM || global.readmessagefrom || "false";
+global.readcmds = process.env.READ_COMMAND || global.readcmds || "true";
+
+let status = false,
+  times = 0;
+smd({ on: "main" }, async (message, text, { icmd }) => {
+  try {
+    if (message.status) return;
+    if (
+      `${global.readmessagefrom}`.includes(message.senderNum) ||
+      ["yes", "true", "ok", "sure"].includes(global.readmessage) ||
+      (icmd && ["yes", "true", "ok", "sure"].includes(global.readcmds))
+    )
+      message.bot.readMessages([message.key]);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+smd({ on: "text" }, async (message, text, { icmd }) => {
+  try {
+    if (
+      ["unavailable", "available", "composing", "recording", "paused"].includes(
+        waPresence
+      )
+    )
+      message.bot.sendPresenceUpdate(waPresence, message.from);
+    if (message.isAstro && !message.fromMe && !message.text.startsWith("$"))
+      message.react("👑");
+  } catch (e) {
+    console.log(e);
+  }
+});
