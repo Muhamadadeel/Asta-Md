ee629d406688f5e3acb98d80158230c8783d7c2c
Astropeda <astromedia0010@gmail.com>
2024-03-30 17:06:35 +0100

    Update set_online.js

--- plugins/set_online.js
+++ plugins/set_online.js
@@ -11,18 +11,16 @@ smd(
   async (message, match) => {
     try {
       if (!match)
-        return await message.send(
-          `_*Example:-* ${Config.HANDLERS}online all`,
-        );
+        return await message.send(`_*Example:-* ${Config.HANDLERS}online all`);
       const available_privacy = ["all", "match_last_seen"];
       if (!available_privacy.includes(match))
         return await message.send(
-          `_action must be *${available_privacy.join("/")}* values_`,
+          `_action must be *${available_privacy.join("/")}* values_`
         );
       await message.bot.updateOnlinePrivacy(match);
       await message.send(`_Privacy Updated to *${match}*_`);
     } catch (e) {
       message.error(`${e}\n\nCommand : online`, e, false);
     }
-  },
+  }
 );
