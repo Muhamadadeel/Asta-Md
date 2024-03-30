86a51c320be5e88edd4e3ce80ea1fd01d5947cba
Astropeda <astromedia0010@gmail.com>
2024-03-30 17:06:42 +0100

    Update unarchive_chat.js

--- plugins/unarchive_chat.js
+++ plugins/unarchive_chat.js
@@ -1,21 +1,29 @@
-let {smd , prefix,Config} = require("../lib")
+let { smd, prefix, Config } = require("../lib");
 
-smd({
-	pattern: 'unarchive',
-	fromMe: true,
-	desc: 'unarchive whatsapp chat',
-	type: 'whatsapp'
-}, async (message, match) => {
-    try{
-	const lstMsg = {
-		message: message.message,
-		key: message.key,
-		messageTimestamp: message.messageTimestamp
-	};
-	await message.bot.chatModify({
-		archive: false,
-		lastMessages: [lstMsg]
-	}, message.jid);
-	await message.send('_Unarchived_')
-}catch(e){ message.error(`${e}\n\nCommand : unarchive` , e, false) }
-})
+smd(
+  {
+    pattern: "unarchive",
+    fromMe: true,
+    desc: "unarchive whatsapp chat",
+    type: "whatsapp",
+  },
+  async (message, match) => {
+    try {
+      const lstMsg = {
+        message: message.message,
+        key: message.key,
+        messageTimestamp: message.messageTimestamp,
+      };
+      await message.bot.chatModify(
+        {
+          archive: false,
+          lastMessages: [lstMsg],
+        },
+        message.jid
+      );
+      await message.send("_Unarchived_");
+    } catch (e) {
+      message.error(`${e}\n\nCommand : unarchive`, e, false);
+    }
+  }
+);
