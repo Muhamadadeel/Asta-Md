fe36399e9105388fe7313c23b44ac9a00ae460da
Astropeda <astromedia0010@gmail.com>
2024-04-03 15:54:30 +0100

    fixing

---
+++ index.js
@@ -0,0 +1,16 @@
+const bot = require(__dirname + '/lib/astro')
+const { VERSION } = require(__dirname + '/config')
+
+const start = async () => {
+    Debug.info(`Asta ${VERSION}`)
+  try {
+    await bot.init()
+    bot.logger.info('ꜱᴛᴀʀᴛɪɴɢ ᴀᴘᴘ')
+    await bot.DATABASE.sync()
+    await bot.connect()
+  } catch (error) {
+    Debug.error(error);
+    start();
+  }
+}
+start();
