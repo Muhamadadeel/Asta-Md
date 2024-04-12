ebab17fd4e47da1809549fbe01cf7ff5ae8ea227
Astropeda <astromedia0010@gmail.com>
2024-04-12 18:06:35 +0100

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -2,57 +2,38 @@ let maher_api = "https://api.maher-zubair.tech/";
 let baseApi =
   process.env.API_SMD || global.api_smd || "https://api-smd-1.vercel.app";
 const { smd, smdJson, prefix, Config } = require("../lib");
-smd(
-  {
-    pattern: "tiktok",
-    alias: ["tt", "ttdl"],
-    desc: "Downloads Tiktok Videos Via Url.",
-    category: "downloader",
-    filename: __filename,
-    use: "<add tiktok url.>",
-  },
-  async (message, url) => {
-    try {
-      var format = url.toLowerCase().includes("doc")
-        ? "document"
-        : url.toLowerCase().includes("mp3")
-        ? "audio"
-        : "video";
-      if (!url) {
-        return await message.reply(
-          `*Uhh Please, Provide me tiktok Video Url*\n*_Ex ${prefix}tiktok https://vm.tiktok.com/ZMMxTk5qr/_*`
-        );
-      }
-      let videoUrl = url ? url.split(" ")[0] : "";
-      if (!/tiktok/.test(videoUrl)) {
-        return await message.reply(
-          "*Uhh Please, Give me Valid Tiktok Video Url!*"
-        );
-      }
-      var isVideoFound = false;
-      try {
-        let response = await smdJson(
-          `${maher_api}download/tiktok2?url=${videoUrl}`
-        );
-        isVideoFound = (response && response?.video?.noWatermark) || false;
-      } catch (error) {
-        let response = await smdJson(
-          `${baseApi}/api/musically?url=${videoUrl}`
-        );
-        isVideoFound = (response && response?.result?.video) || false;
-      }
-      if (isVideoFound) {
-        return await message.send(
-          isVideoFound,
-          { caption: Config.caption },
-          format,
-          message
-        );
-      } else {
-        return await message.reply("Error While Downloading Your Video");
-      }
-    } catch (error) {
-      return message.error(`${error}\n\ncommand: tiktok`, error);
-    }
-  }
-);
+smd({
+ pattern: "tiktok",
+ alias: ["tt", "ttdl"],
+ desc: "Downloads Tiktok Videos Via Url.",
+ category: "downloader",
+ filename: __filename,
+ use: "<add tiktok url.>"
+}, async (bot, message, url, { cmdName }) => {
+ try {
+   let videoUrl = url.split(" ")[0];
+     if (!/tiktok/.test(videoUrl)) {
+       return await message.send("*_Please Give Me Valid Tiktok Video Link..!_*");
+     }
+
+     let response = await axios.get(`${maher_api}download/tiktok2?url=${encodeURIComponent(videoUrl)}`);
+     const data = response.data;
+     const videoNoWatermark = data.result.url.nowm;
+
+     await message.sent("*_Downloading Your Video Without WaterMark..._*");
+
+     let videoMessage = {
+       video: { url: videoNoWatermark },
+       caption: "Tiktok Downloader",
+       headerType: 4,
+       contextInfo: {
+        isfowarded: false
+       }
+     };
+
+     await bot.sendMessage(message.chat, videoMessage, { quoted: message });
+ } catch (error) {
+   message.sent(`${serror} *_User ${arr} ${sm}${message.sender.split("@")[0]}_*\n*_Command ${arr} ${cmdName}_*\n\n*_Error Occured While Downloading Your Media_*\n_${error}_`, { quoted: message });
+   console.log(error);
+ }
+});
