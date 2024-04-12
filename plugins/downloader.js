d9b2c8204c99fb5621ae93d0f489c5f26ec5cf1f
Astropeda <astromedia0010@gmail.com>
2024-04-12 19:15:30 +0100

    fixed

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -1,5 +1,6 @@
 let baseApi =
   process.env.API_SMD || global.api_smd || "https://api-smd-1.vercel.app";
+let maher_api = "https://api.maher-zubair.tech/";
 const {
   smd,
   fetchJson,
@@ -22,6 +23,33 @@ const axios = require("axios");
 const fetch = require("node-fetch");
 var videotime = 2000;
 const { cmd } = require("../lib/plugins");
+smd(
+  {
+    pattern: "tiktok",
+    alias: ["tt", "ttdl"],
+    desc: "Downloads Tiktok Videos Via Url.",
+    category: "downloader",
+    filename: __filename,
+    use: "<add tiktok url.>",
+  },
+  async (bot, message, url) => {
+    try {
+      let videoUrl = url.split(" ")[0];
+      if (!/tiktok/.test(videoUrl)) {
+        return await message.send("*Please Give Me Valid Tiktok Video Link*");
+      }
+      let response = await axios.get(`${maher_api}download/tiktok2?url=${videoUrl}`);
+      const data = response.data;
+      const video = data.result.url.nowm;
+      const res = await fetch(video);
+      const buffer = await res.buffer();
+      await bot.send(message.chat, buffer); 
+    } catch (error) {
+      message.send(`\n*_Error Occured While Downloading Your Media_*\n_${error}_`);
+      console.log(error);
+    }
+  }
+);
 smd(
   {
     pattern: "tgs",
