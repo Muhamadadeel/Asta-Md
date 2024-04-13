39d02991fa334e4d360fd33e4684e69500c849b4
Astropeda <astromedia0010@gmail.com>
2024-04-13 09:41:22 +0100

    Update hack.js

--- plugins/hack.js
+++ plugins/hack.js
@@ -1,62 +1,55 @@
-const {
-    smd,
-    prefix, 
-    Config ,
-    sleep
-     } = require('../lib')
+const { smd, prefix, Config, sleep } = require("../lib");
 
-
-
-
-
-
-
-smd({
-    cmdname: "hack",    
-    type: "fun",    
-    info: "hacking prank",    
+smd(
+  {
+    cmdname: "hack",
+    type: "fun",
+    info: "hacking prank",
     filename: __filename,
+  },
 
-},
+  async (citel) => {
+    await citel.send("`Downloading Malware`");
+    await sleep(2000);
+    await citel.send("`Running Malware`");
+    await sleep(2000);
+    await citel.send("`Encrptying System files _0x3bbd3`");
+    await sleep(600);
+    await citel.send(" █ █ 20%");
+    await sleep(600);
+    await citel.send(" █ █ █ 30%");
+    await sleep(660);
+    await citel.send(" █ █ █ █ 40%");
+    await sleep(700);
+    await citel.send(" █ █ █ █ █ 50%");
+    await sleep(750);
+    await citel.send("`Sent Data to Operators`");
+    await sleep(800);
+    await citel.send(" █ █ █ █ █ █ █ 70%");
+    await sleep(850);
+    await citel.send(" █ █ █ █ █ █ █ █ 80%");
+    await sleep(900);
+    await citel.send(" █ █ █ █ █ █ █ █ █ 90%");
+    await sleep(950);
+    await citel.send(" █ █ █ █ █ █ █ █ █ █ 100%");
+    await sleep(1100);
+    await citel.send(
+      "`Sent All Information to Remote Server` ✅"
+    );
+    await sleep(1000);
+    await citel.send("`Data Successfully Transferred` ✅");
+    await sleep(1000);
+    await citel.send(
+      "`Stoping Malware Service` ✅"
+    );
+    await sleep(1000);
+    await citel.send(" HACKING COMPLETED ");
+    await sleep(2000);
+    await citel.send(" SENDING LOG DOCUMENTS...");
+    await sleep(1000);
+    await citel.send(" SUCCESSFULLY SENT DATA AND Connection disconnected");
+    await sleep(1000);
 
-async(citel) => {    
-await citel.send("Injecting Malware")   
-await sleep(2000)    
-await citel.send(" █ 10%")    
-await sleep(1000)    
-await citel.send(" █ █ 20%")    
-await sleep(1000)    
-await citel.send(" █ █ █ 30%")    
-await sleep(1000)    
-await citel.send(" █ █ █ █ 40%")    
-await sleep(1000)    
-await citel.send(" █ █ █ █ █ 50%")    
-await sleep(1000)    
-await citel.send(" █ █ █ █ █ █ 60%")    
-await sleep(1000)    
-await citel.send(" █ █ █ █ █ █ █ 70%")    
-await sleep(1000)    
-await citel.send(" █ █ █ █ █ █ █ █ 80%")    
-await sleep(1000)    
-await citel.send(" █ █ █ █ █ █ █ █ █ 90%")    
-await sleep(1000)    
-await citel.send(" █ █ █ █ █ █ █ █ █ █ 100%")    
-await sleep(1000)    
-await citel.send("System hyjacking on process.. \n Conecting to Server error to find 404 ")    
-await sleep(1000)    
-await citel.send("Device successfully connected... \n Riciving data...")    
-await sleep(1000)    
-await citel.send("Data hyjacked from divice 100% completed \n killing all evidence killing all malwares...")
-await sleep(1000)    
-await citel.send(" HACKING COMPLETED ")    
-await sleep(2000)    
-await citel.send(" SENDING LOG DOCUMENTS...")    
-await sleep(1000)
-await citel.send(" SUCCESSFULLY SENT DATA AND Connection disconnected")    
-await sleep(1000)
-
-    return await citel.send('BACKLOGS CLEARED');
-
-}
-
-)
+    return await citel.send("BACKLOGS CLEARED");
+  }
+);
