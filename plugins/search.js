7904141d3f6b12d74c68651fc9dfe0f8d324782c
Astropeda <astromedia0010@gmail.com>
2024-04-19 07:19:51 +0100

    Update search.js

--- plugins/search.js
+++ plugins/search.js
@@ -184,7 +184,7 @@ smd({
     if (!res.status === 200) return message.send("*There's a problem, try again later!*");
 
     const { result } = res;
-    const tbl = "𝘼𝙎𝙏𝘼 𝙂𝙋𝙏4 𝙏𝙐𝙍𝘽𝙊\n```";
+    const tbl = "\n𝘼𝙎𝙏𝘼 𝙂𝙋𝙏4 𝙏𝙐𝙍𝘽𝙊\n```";
     await send(message, `${tbl}${result}${tbl}`);
   } catch (e) {
     return await message.error(`${e}\n\n command: ${cmdName}`, e, `*_An error occurred while processing your request_*`);
