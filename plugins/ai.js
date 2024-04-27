1e498b1a7ec5d4e621d433709dc168e89a104f87
Astropeda <astromedia0010@gmail.com>
2024-04-27 10:47:47 +0100

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -300,26 +300,22 @@ smd(
     }
   }
 );
-smd({
-  pattern: "gpt4",
-  category: "ai",
-  desc: "Chat with GPT-4 AI model",
-  use: "<text>",
-  filename: __filename,
-}, async (message, text, { cmdName }) => {
+smd({ pattern: "gpt4", category: "ai", desc: "Chat with GPT-4 AI model", use: "<text>", filename: __filename, }, async (message, text, { cmdName }) => {
   if (!text) return message.reply(`*_Please provide a query_*\n*_Example ${prefix + cmdName} What is the meaning of life?_*`);
 
   try {
-    const apiUrl = `https://ultimetron.guruapi.tech/gpt4?prompt=${encodeURIComponent(text)}`;
+    const apiUrl = `https://api.maher-zubair.tech/ai/chatgptv4?q=${encodeURIComponent(text)}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
 
-    if (!data.result.success) return message.send("*There's a problem, try again later!*");
+    if (data.status !== 200) {
+      return message.send("*There's a problem, try again later!*");
+    }
 
-    const { reply } = data.result;
+    const { result } = data;
     const astro = "ᴀsᴛᴀ ɢᴘᴛ𝟺\n";
     const tbl = "```";
-    await send(message, `${astro}${tbl}${reply}${tbl}`);
+    await send(message, `${astro}${tbl}${result}${tbl}`);
   } catch (error) {
     return await message.error(`${error}\n\n command: ${cmdName}`, error, `*_An error occurred while processing your request_*`);
   }
