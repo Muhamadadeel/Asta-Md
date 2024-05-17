ee8770a7c82f95a903a54631e789ab1ce948e883
Astropeda <astromedia0010@gmail.com>
2024-05-17 13:19:54 +0100

    open source

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ global.readmessagefrom = process.env.READ_MESSAGE_FROM || "2348039607375";
 
 global.read_status = process.env.AUTO_READ_STATUS || "false"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
-global.save_status_from =  process.env.SAVE_STATUS_FROM  || "2348039607375";
+global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null";
 global.read_status_from =  process.env.READ_STATUS_FROM  ||  "2348039607375";
 
 global.api_smd = "https://api-smd.onrender.com"
