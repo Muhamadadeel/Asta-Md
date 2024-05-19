763078b32b022e9072dafbf1627d5c0ff1e122e4
Astropeda <astromedia0010@gmail.com>
2024-05-19 15:40:35 +0100

    new sweet update

--- plugins/_sys.js
+++ plugins/_sys.js
@@ -1,70 +1,105 @@
-const {
-    plugins,
-    smd,
-    Config,
-    smdJson,
-    smdBuffer
-  } = require("../lib");
-  let s_ser = true;
-  smd({
+const { plugins, Config } = require("../lib");
+const {AdminFunction} = require("../lib")
+AdminFunction(
+  {
     cmdname: "restart",
     info: "To restart bot",
     type: "tools",
-    fromMe: s_ser,
-    filename: __filename
-  }, async _0x514d3c => {
-    const {
-      exec: _0x1912df
-    } = require("child_process");
-    _0x514d3c.reply("Restarting");
-    _0x1912df("pm2 restart all");
-  });
-  smd({
+    fromMe: true,
+    filename: __filename,
+  },
+  async (action) => {
+    const { exec: restarter } = require("child_process");
+    action.reply("*`Restarting Bot`*");
+    restarter("pm2 restart all");
+  }
+);
+AdminFunction(
+  {
+    cmdname: "shutdown",
+    info: "To Shutdown bot",
+    type: "tools",
+    fromMe: true,
+    filename: __filename,
+  },
+  async (action) => {
+    const { exec: killer } = require("child_process");
+    action.reply("*`Shutting Down Bot`*");
+    killer("pm2 stop all");
+  }
+);
+AdminFunction(
+  {
     cmdname: "plugins",
     alias: ["plugin"],
     type: "owner",
     info: "Shows list of all externally installed modules",
-    fromMe: s_ser,
+    fromMe: true,
     filename: __filename,
-    use: "<name>"
-  }, async (_0x2a10d6, _0x2420b0) => {
+    use: "<name>",
+  },
+  async (message, InstalledPlugins) => {
     try {
-      let _0x4e5e2e = await plugins(_0x2a10d6, "plugins", _0x2420b0);
-      return await _0x2a10d6.send(!_0x4e5e2e ? "*_There's no plugin install in " + Config.botname + "_*" : !_0x2420b0 ? "*All Installed Modules are:-*\n\n" + _0x4e5e2e : _0x4e5e2e);
+      let Installer = await plugins(message, "plugins", InstalledPlugins);
+      return await message.send(
+        !Installer
+          ? "*_There's no plugin install in " + Config.botname + "_*"
+          : !InstalledPlugins
+          ? "*All Installed Modules are:-*\n\n" + Installer
+          : Installer
+      );
     } catch (_0x21e335) {
-      _0x2a10d6.error(_0x21e335 + " \n\ncmdName plugins\n");
+      message.error(_0x21e335 + " \n\ncmdName plugins\n");
     }
-  });
-  smd({
+  }
+);
+AdminFunction(
+  {
     pattern: "remove",
     alias: ["uninstall"],
     type: "owner",
     info: "removes external modules.",
-    fromMe: s_ser,
+    fromMe: true,
     filename: __filename,
-    use: "<plugin name>"
-  }, async (_0x1510c9, _0x40e763) => {
-    if (!_0x40e763) {
-      return await _0x1510c9.reply("*_Uhh Please, Provide Me Plugin Name_*");
+    use: "<plugin name>",
+  },
+  async (message, IsName) => {
+    if (!IsName) {
+      return await message.reply("*`Sir I Need A Plugin Name, To Remove A Plugin`*");
     }
-    if (_0x40e763 === "alls") {
-      return await _0x1510c9.reply(await plugins("remove", "all", __dirname));
+    if (IsName === "alls") {
+      return await message.reply(await plugins("remove", "all", __dirname));
     }
     try {
-      await _0x1510c9.send(await plugins(_0x1510c9, "remove", _0x40e763, __dirname), {}, "", _0x1510c9);
+      await message.send(
+        await plugins(message, "remove", IsName, __dirname),
+        {},
+        "",
+        message
+      );
     } catch {}
-  });
-  smd({
+  }
+);
+AdminFunction(
+  {
     cmdname: "install",
     type: "owner",
     info: "Installs external modules..",
-    fromMe: s_ser,
+    fromMe: true,
     filename: __filename,
-    use: "<gist url>"
-  }, async (_0xf71b5c, _0x2bdd09) => {
-    let _0x2b0828 = _0x2bdd09 ? _0x2bdd09 : _0xf71b5c.quoted ? _0xf71b5c.quoted.text : "";
-    if (!_0x2b0828.toLowerCase().includes("https")) {
-      return await _0xf71b5c.send("*_Uhh Please, Provide Me Plugin Url_*");
+    use: "<gist url>",
+  },
+  async (message, IsUrl) => {
+    let Installer = IsUrl
+      ? IsUrl
+      : message.quoted
+      ? message.quoted.text
+      : "";
+    if (!Installer.toLowerCase().includes("https")) {
+      return await message.send("*`Sir Please Give A Vaild Plugin Url To Install From`*");
     }
-    await _0xf71b5c.reply(await plugins(_0xf71b5c, "install", _0x2b0828, __dirname));
-  });
+    await message.reply(
+      await plugins(message, "install", Installer, __dirname)
+    );
+  }
+);
