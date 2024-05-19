be7c2c02dc40cc33a318596f470375aad1a4d427
Astropeda <astromedia0010@gmail.com>
2024-05-19 12:29:05 +0100

    fixed

--- plugins/filter.js
+++ plugins/filter.js
@@ -1,4 +1,4 @@
-const { smd } = require(lib_dir);
+const { smd, amd } = require(lib_dir);
 const { filter } = require(lib_dir + "/astropeda.js");
 
 smd(
@@ -14,5 +14,20 @@ smd(
   },
   async (message, match) => {
     filter.set(message, match);
+  }
+);
+amd(
+  {
+    pattern: "fstop",
+    category: "chats",
+    desc: "stop auto reply from a word",
+    use: "[ asta : how can i help you! ]",
+    usage:
+      "stop filter message to specific word, That already set in filter text!",
+    fromMe: true,
+    filename: __filename,
+  },
+  async (message, match) => {
+    filter.stop(message, match);
   }
 );
