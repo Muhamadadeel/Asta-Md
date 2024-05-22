c8d9f263f7ee54fb9533adfc5750bdaec8a31508
Astropeda <astromedia0010@gmail.com>
2024-05-22 20:17:03 +0100

    join fucntionality

--- plugins/group_chats.js
+++ plugins/group_chats.js
@@ -1,7 +1,7 @@
 const { updateProfilePicture, parsedJid } = require("../lib");
 const {
   sck,
-  smd,
+  UserFunction,
   send,
   Config,
   tlang,
@@ -12,3 +12,50 @@ const {
 const ᴀsᴛᴀ_ᴍᴅ = require("../lib/plugins");
 const { cmd } = ᴀsᴛᴀ_ᴍᴅ;
 const grouppattern = /https:\/\/chat\.whatsapp\.com\/[A-Za-z0-9]{22}/g;
+UserFunction(
+  {
+    cmdname: "join",
+    info: "joins group by link",
+    type: "whatsapp",
+    fromMe: true,
+    filename: __filename,
+    use: "<group link.>",
+  },
+  async (message, context) => {
+    try {
+      if (message.reply_message && message.reply_message.groupInvite) {
+        var InviteLink = await message.bot.groupAcceptInviteV4(
+          message.chat,
+          message.reply_message.msg
+        );
+        if (InviteLink && InviteLink.includes("joined to:")) {
+          return await send(message, "*Joined Group*", {}, "", message);
+        }
+      }
+      let link = context ? context : message.reply_text;
+      const match = link.match(grouppattern);
+      if (!match) {
+        return await message.reply(
+          "*Give Me A Vaild WhatsApp Group Link To Join*\n\n*" +
+            prefix +
+            "join `Your Link`*"
+        );
+      }
+      let MatchInfo = match[0].split("https://chat.whatsapp.com/")[1].trim();
+      await message.bot
+        .groupAcceptInvite(MatchInfo)
+        .then((_0x7f3222) => send(message, "*_Joined_*", {}, "", message))
+        .catch((error) =>
+          message.send(
+            "*Unable To Join, this is not a vaild whatsapp Group Link."
+          )
+        );
+    } catch (error) {
+      await message.error(
+        error + "\n\ncommand: join",
+        error,
+        "*Can't Join, Group Id not found*"
+      );
+    }
+  }
+);
