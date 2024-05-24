95838ddf64d4e135f8e74075fe620df6a315b1e7
Astropeda <astromedia0010@gmail.com>
2024-05-24 09:27:23 +0100

    more error handling techques

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-const bot = require(__dirname + '/lib/amd')
+const bot = require(__dirname + '/lib/amd');
+
+const start = async () => {
+  try {
+    await bot.init();
+    await bot.DATABASE.sync();
+    await bot.connect();
+    console.log('Bot started successfully!');
+  } catch (error) {
+    console.error('Error starting bot:', error);
+    setTimeout(() => {
+      console.log('Attempting to restart bot...');
+      start();
+    }, 5000);
+  }
+};
+
+start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DEPRECATED BELOW
+/**const bot = require(__dirname + '/lib/amd')
 
 const start = async () => {
   try {
@@ -11,3 +43,4 @@ const start = async () => {
   }
 }
 start();
+*/
