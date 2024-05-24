2a6da06f2c2d9e80363347a2ac62b6f242d937d7
Astropeda <astromedia0010@gmail.com>
2024-05-24 13:56:03 +0100

    fakereply

--- plugins/extras.js
+++ plugins/extras.js
@@ -1,4 +1,4 @@
-const { smd, tlang, prefix } = require("../lib");
+const { UserFunction, tlang, prefix } = require("../lib");
 async function loadMessages(msg, data, match = "") {
   try {
     match = (match ? match : data).split("@")[0];
@@ -15,7 +15,7 @@ async function loadMessages(msg, data, match = "") {
     return [];
   }
 }
-smd(
+UserFunction(
   {
     pattern: "delspam",
     category: "chats",
@@ -60,6 +60,51 @@ smd(
       }
     } catch (err) {
       console.log(err);
+    }
+  }
+);
+smd(
+  {
+    cmdname: "freply",
+    desc: "Create fake Reply by given texts!",
+    type: "chats",
+    use: " msg| reply_text | number ",
+    usage: "generates fake messages of given text and number!",
+    filename: __filename,
+    public: true,
+  },
+  async (m, text) => {
+    try {
+      let types = ["text", "order", "contact", "image", "video"];
+      let args = text.split("|");
+      if (!text || args.length < 3)
+        return await m.reply(
+          `*Use ${prefix}fakereply text |Reply_text|2349027862116|type(text,order,contact,image,video)*`
+        );
+      let reply = args[0],
+        msg = args[1],
+        num = `${args[2].replace(/[^0-9]/g, "")}@s.whatsapp.net`,
+        type = args[3] && types.includes(args[3]) ? args[3] : "text",
+        charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        smds = "SMD";
+      for (let i = 0; i < 13; i++) {
+        smds += charset[Math.floor(Math.random() * charset.length)];
+      }
+      let fak = await m.bot.fakeMessage(
+        type,
+        { id: smds, remoteJid: m.isGroup ? m.chat : num, participant: num },
+        msg
+      );
+      try {
+        if (type === "contact") {
+          fak.message.contactMessage.jpegThumbnail = await m.getpp(num);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+      await m.bot.sendMessage(m.chat, { text: reply }, { quoted: fak });
+    } catch (e) {
+      m.error(`${e}\n\nCommand: fakereply`, e, false);
     }
   }
 );
