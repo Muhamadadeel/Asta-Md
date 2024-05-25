f2cc71ef48d54c913556a7a7c124c809aa4f331d
Astropeda <astromedia0010@gmail.com>
2024-05-25 05:20:01 +0100

    fixed

--- plugins/misc.js
+++ plugins/misc.js
@@ -1,16 +1,24 @@
-let {UserFunction, fetchJson, amdJson,} =require("../lib")
-let fetch = require('node-fetch') || fetchJson || amdJson
+let { UserFunction, fetchJson, amdJson } = require("../lib");
+let fetch = require("node-fetch") || fetchJson || amdJson;
 UserFunction(
   {
     pattern: "gen",
-    desc: "Generate random card details.",
-    category: "misc",
+    desc: "Generate fake card information.",
+    category: "generator",
     filename: __filename,
-    use: "",
+    use: "<query>",
   },
-  async (m) => {
+  async (m, query) => {
     try {
-      const apiUrl = "https://api.maher-zubair.tech/misc/bingen?query=44270700";
+      if (!query) {
+        return await m.send(
+          "*_Please provide a query for the card generator!_*"
+        );
+      }
+
+      const apiUrl = `https://api.maher-zubair.tech/misc/bingen?query=${encodeURIComponent(
+        query
+      )}`;
       const response = await fetch(apiUrl);
 
       if (!response.ok) {
@@ -25,17 +33,18 @@ UserFunction(
         return await m.send("*_An error occurred while fetching the data._*");
       }
 
-      const { result } = data;
-      const randomCard = result[Math.floor(Math.random() * result.length)];
-      const { CardNumber, ExpirationDate, CVV } = randomCard;
+      const cards = data.result;
+      let message = "*Generated Card Information*\n\n";
 
-      const cardDetails = `
-*Card Number:* ${CardNumber}
-*Expiration Date:* ${ExpirationDate}
-*CVV:* ${CVV}
-`;
+      cards.forEach((card) => {
+        message += `
+          *Card Number:* ${card.CardNumber}
+          *Expiration Date:* ${card.ExpirationDate}
+          *CVV:* ${card.CVV}\n
+          `;
+      });
 
-      await m.send(cardDetails);
+      await m.send(message);
     } catch (e) {
       await m.error(`${e}\n\ncommand: gen`, e);
     }
