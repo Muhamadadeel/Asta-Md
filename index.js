7df86acf2d218cefc5aa865a2e862fcf66cda039
Astropeda <astromedia0010@gmail.com>
2024-05-25 15:36:18 +0100

    more

--- index.js
+++ index.js
@@ -16,31 +16,3 @@ const start = async () => {
 };
 
 start();
-
-
-
-
-
-
-
-
-
-
-
-
-
-// DEPRECATED BELOW
-/**const bot = require(__dirname + '/lib/amd')
-
-const start = async () => {
-  try {
-    await bot.init()
-    await bot.DATABASE.sync()
-    await bot.connect()
-  } catch (error) {
-    Debug.error(error);
-    start();
-  }
-}
-start();
-*/
