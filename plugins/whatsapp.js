f2bc75fcac7bffff98f4c3ff561503cc33dcea46
Astropeda <astromedia0010@gmail.com>
2024-05-26 15:32:00 +0100

    updtae

--- plugins/whatsapp.js
+++ plugins/whatsapp.js
@@ -13,73 +13,6 @@ let counter_name = { name: "asta-md" }
 try { global.Package_ = JSON.parse(require('fs').readFileSync('package.json', 'utf8')) || counter_name } catch { }
 
 UserFunction({
-  pattern: "save",
-  desc: "Save whatsapp status",
-  category: "whatsapp",
-  filename: __filename,
-  use: "< status >",
-}, async (message) => {
-  try {
-    let mm = message.reply_message && message.reply_message.status ? message.reply_message : false;
-    if (mm) { message.bot.forwardOrBroadCast(message.user, mm, { quoted: { key: mm.key, message: mm.message } }) }
-    else message.send("*reply to whatsapp status*")
-  } catch (e) { await message.error(`${e}\n\ncommand : #(Status Saver)`, e, false) }
-})
-global.waPresence = process.env.WAPRESENCE && process.env.WAPRESENCE === "online" ? "available" : process.env.WAPRESENCE || "";
-global.config_dir = require("path").join(__dirname, '../', './config')
-global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaPGt3QEwEjpBXT4Rv0z";
-
-try {
-
-  return
-  global.auto_send_status = process.env.AUTO_SEND_STATUS || 'true';
-
-
-  const regexSend = new RegExp(`\\b(?:${["send", "share", "snd", "give", "save", "sendme", "forward", "fwd"].join('|')})\\b`, 'i');
-  UserFunction(
-    { on: "quoted" },
-    async (message, text) => {
-      try {
-        let mm = message.reply_message.status ? message.reply_message : false;
-        if (mm && regexSend.test(text.toLowerCase())) {
-          if (global.auto_send_status == "true") message.bot.forwardOrBroadCast(message.fromMe ? message.user : message.from, mm, { quoted: { key: mm.key, message: mm.message } })
-        }
-      } catch (e) { console.log(e) }
-    })
-  let status = false, times = 0;
-  UserFunction(
-    { on: "main" },
-    async (message, text, { icmd }) => {
-      try {
-        if (!status) {
-          try {
-          } catch (e) { }
-        }
-
-        if (message.status) return
-        if (`${global.readmessagefrom}`.includes(message.senderNum) || ["yes", "true", "ok", "sure"].includes(global.readmessage) || (icmd && ["yes", "true", "ok", "sure"].includes(global.readcmds))) message.bot.readMessages([message.key])
-      } catch (e) { console.log(e) }
-    })
-
-  UserFunction(
-    { on: "text" },
-    async (message, text, { icmd }) => {
-      try {
-        if (['unavailable', 'available', 'composing', 'recording', 'paused'].includes(waPresence)) message.bot.sendPresenceUpdate(waPresence, message.from)
-      } catch (e) { console.log(e) }
-    })
-  UserFunction(
-    { on: "status" },
-    async (message, text) => {
-      try {
-        if (`${global.read_status_from}`.split(",").includes(message.key.participant.split("@")[0]) || ["yes", "true", "ok", "sure"].includes(global.read_status) || message.fromMe || message.isSuhail) { await message.bot.readMessages([{ ...message.key, fromMe: false }]) }
-        if ((`${global.save_status_from}`.split(",").includes(message.key.participant.split("@")[0]) || ["yes", "true", "ok", "sure"].includes(global.save_status)) && !message.fromMe) {
-          await message.bot.forwardOrBroadCast(message.user, message, { quoted: { key: message.key, message: message.message }, })
-        }
-      } catch (e) { console.log(e) }
-    })
-} catch (e) { }
-UserFunction({
   pattern: "jid",
   desc: "Get JID of a user in a group.",
   category: "user",
@@ -187,3 +120,71 @@ UserFunction({
     return await message.reply("https://wa.me/" + message.sender.split("@")[0]);
   } catch { }
 });
+
+UserFunction({
+  pattern: "save",
+  desc: "Save whatsapp status",
+  category: "whatsapp",
+  filename: __filename,
+  use: "< status >",
+}, async (message) => {
+  try {
+    let mm = message.reply_message && message.reply_message.status ? message.reply_message : false;
+    if (mm) { message.bot.forwardOrBroadCast(message.user, mm, { quoted: { key: mm.key, message: mm.message } }) }
+    else message.send("*reply to whatsapp status*")
+  } catch (e) { await message.error(`${e}\n\ncommand : #(Status Saver)`, e, false) }
+})
+global.waPresence = process.env.WAPRESENCE && process.env.WAPRESENCE === "online" ? "available" : process.env.WAPRESENCE || "";
+global.config_dir = require("path").join(__dirname, '../', './config')
+global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaPGt3QEwEjpBXT4Rv0z";
+
+try {
+
+  return
+  global.auto_send_status = process.env.AUTO_SEND_STATUS || 'true';
+
+
+  const regexSend = new RegExp(`\\b(?:${["send", "share", "snd", "give", "save", "sendme", "forward", "fwd"].join('|')})\\b`, 'i');
+  UserFunction(
+    { on: "quoted" },
+    async (message, text) => {
+      try {
+        let mm = message.reply_message.status ? message.reply_message : false;
+        if (mm && regexSend.test(text.toLowerCase())) {
+          if (global.auto_send_status == "true") message.bot.forwardOrBroadCast(message.fromMe ? message.user : message.from, mm, { quoted: { key: mm.key, message: mm.message } })
+        }
+      } catch (e) { console.log(e) }
+    })
+  let status = false, times = 0;
+  UserFunction(
+    { on: "main" },
+    async (message, text, { icmd }) => {
+      try {
+        if (!status) {
+          try {
+          } catch (e) { }
+        }
+
+        if (message.status) return
+        if (`${global.readmessagefrom}`.includes(message.senderNum) || ["yes", "true", "ok", "sure"].includes(global.readmessage) || (icmd && ["yes", "true", "ok", "sure"].includes(global.readcmds))) message.bot.readMessages([message.key])
+      } catch (e) { console.log(e) }
+    })
+
+  UserFunction(
+    { on: "text" },
+    async (message, text, { icmd }) => {
+      try {
+        if (['unavailable', 'available', 'composing', 'recording', 'paused'].includes(waPresence)) message.bot.sendPresenceUpdate(waPresence, message.from)
+      } catch (e) { console.log(e) }
+    })
+  UserFunction(
+    { on: "status" },
+    async (message, text) => {
+      try {
+        if (`${global.read_status_from}`.split(",").includes(message.key.participant.split("@")[0]) || ["yes", "true", "ok", "sure"].includes(global.read_status) || message.fromMe || message.isSuhail) { await message.bot.readMessages([{ ...message.key, fromMe: false }]) }
+        if ((`${global.save_status_from}`.split(",").includes(message.key.participant.split("@")[0]) || ["yes", "true", "ok", "sure"].includes(global.save_status)) && !message.fromMe) {
+          await message.bot.forwardOrBroadCast(message.user, message, { quoted: { key: message.key, message: message.message }, })
+        }
+      } catch (e) { console.log(e) }
+    })
+} catch (e) { }
